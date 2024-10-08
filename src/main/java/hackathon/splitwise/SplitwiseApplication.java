ac79a7a7a4f2a708b65e50e5f298974a44cf64f1
yukti2510 <yukti251098@gmail.com>
2024-10-08 16:46:33 +0530

    initial commit

---
+++ src/main/java/hackathon/splitwise/SplitwiseApplication.java
@@ -0,0 +1,12 @@
+package hackathon.splitwise;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
